350fbef936438bfee40c6ec8e9d0895f57ad3e87
John Graham <john@simpli.fi>
2017-08-24 02:26:44 -0500

    grab flags supports neomake now

--- grab_flags.py
+++ grab_flags.py
@@ -1,5 +1,6 @@
-# This script can be used to grab a set of flags from a compile_commands.json file and put them in a syntastic friendly
-# format. Useful for CMAKE projects that you want to get syntastic working on.
+"""This script can be used to grab a set of flags from a
+compile_commands.json file and put them in a syntastic friendly
+format. Useful for CMAKE projects that you want to get syntastic working on."""
 
 import json
 import re
@@ -7,6 +8,8 @@ import re
 IS_IMPORTANT_FLAG = re.compile('std|^-isystem|^-I')
 
 def grab_flags(command):
+    """Grabs all include and warning flags from a line from
+    compile_commands.json files"""
     all_flags = command.split()
     important_flags = set()
     for flag in all_flags:
@@ -18,9 +21,22 @@ IMPORTANT_FLAGS = set()
 for command_json in json.load(open("./build/compile_commands.json")):
     IMPORTANT_FLAGS.update(grab_flags(command_json['command']))
 
-syntastic_file = open(".syntastic_cpp_config", "w")
-for flag in IMPORTANT_FLAGS:
-    syntastic_file.write(flag + "\n")
+SYNTASTIC_FILE = open(".syntastic_cpp_config", "w")
+for syntastic_flag in IMPORTANT_FLAGS:
+    SYNTASTIC_FILE.write(syntastic_flag + "\n")
 
-syntastic_file.close()
+SYNTASTIC_FILE.close()
+
+# TO USE:
+# add
+# source .neomake_cpp_config.vimrc
+# to a directory local .lvimrc file
+# must be used with local vimrc plugin
+# https://github.com/embear/vim-localvimrc
+NEOMAKE_FILE = open(".neomake_cpp_config.vimrc", "w")
+for neomake_flag in IMPORTANT_FLAGS:
+    NEOMAKE_FILE.write("call add(g:neomake_cpp_gcc_maker['args'], \"{0}\")\n"
+        .format(neomake_flag)
+    )
+NEOMAKE_FILE.close()
 
